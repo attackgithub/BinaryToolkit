c09a3c7c02f23ac41c60941c80a223496148465d
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-01-18 22:12:08 +0200

    Implement basic functions

---
+++ MemoryToolkit/Core/ProcessMemory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using MemoryToolkit.Exceptions;
+
+namespace MemoryToolkit
+{
+    public class ProcessMemory : IDisposable
+    {
+        public ProcessMemory(int processId)
+        {
+            Process = Process.GetProcessById(processId);
+        }
+        public ProcessMemory(Process processInstance)
+        {
+            Process = processInstance;
+        }
+        
+
+        private Process process = null;
+        //
+        // <summary>
+        // Selected process
+        // </summary>
+        //
+        public Process Process
+        {
+            get
+            {
+                return process;
+            }
+            set
+            {
+                // Close access to previous process
+                Dispose();
+
+                // Open access to new process
+                process = value;
+
+                if (process.HasExited)
+                    throw new ProcessHasExitedException(process);
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ MemoryToolkit/Stuff/Exceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemoryToolkit.Exceptions
+{
+    public class ProcessHasExitedException : Exception
+    {
+        public System.Diagnostics.Process process = null;
+        public ProcessHasExitedException(System.Diagnostics.Process Process)
+        {
+            process = Process;
+        }
+        public ProcessHasExitedException() { }
+
+        public override string ToString()
+        {
+            if (process == null)
+                return "Process has exited";
+            else
+                return $"Process {process.ProcessName} (pid: {process.Id}) has exited";
+        }
+    }
+}
